65e4391bda3c9192eae950243cc0e8a5f68e188b
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2018-05-03 16:20:32 +0200

    Using openzeppelin-solidity token impl for tests

--- contracts/test/Token_V0.sol
+++ contracts/test/Token_V0.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.21;
 
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "../R8App.sol";
 
 
@@ -9,78 +8,11 @@ import "../R8App.sol";
  * @title Token_V0
  * @dev Version 0 of a token to show upgradeability.
  */
-contract Token_V0 is R8App, Ownable {
-  using SafeMath for uint256;
-
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-
-  uint256 internal _totalSupply;
-  mapping (address => uint256) internal _balances;
-  mapping (address => mapping (address => uint256)) internal _allowances;
+contract Token_V0 is R8App, MintableToken {
 
   function initialize(address sender) public payable {
     super.initialize(sender);
     owner = sender;
   }
 
-  function totalSupply() public view returns (uint256) {
-    return _totalSupply;
-  }
-
-  function balanceOf(address owner) public view returns (uint256) {
-    return _balances[owner];
-  }
-
-  function allowance(address owner, address spender) public view returns (uint256) {
-    return _allowances[owner][spender];
-  }
-
-  function transfer(address to, uint256 value) public returns (bool) {
-    require(to != address(0));
-    require(value <= _balances[msg.sender]);
-    
-    _balances[msg.sender] = _balances[msg.sender].sub(value);
-    _balances[to] = _balances[to].add(value);
-    emit Transfer(msg.sender, to, value);
-    return true;
-  }
-
-  function transferFrom(address from, address to, uint256 value) public returns (bool) {
-    require(to != address(0));
-    require(value <= _balances[from]);
-    require(value <= _allowances[from][msg.sender]);
-
-    _balances[from] = _balances[from].sub(value);
-    _balances[to] = _balances[to].add(value);
-    _allowances[from][msg.sender] = _allowances[from][msg.sender].sub(value);
-    emit Transfer(from, to, value);
-    return true;
-  }
-
-  function approve(address spender, uint256 value) public {
-    _allowances[msg.sender][spender] = value;
-    emit Approval(msg.sender, spender, value);
-  }
-
-  function increaseApproval(address spender, uint256 addedValue) public {
-    _allowances[msg.sender][spender] = _allowances[msg.sender][spender].add(addedValue);
-    emit Approval(msg.sender, spender, _allowances[msg.sender][spender]);
-  }
-
-  function decreaseApproval(address spender, uint256 subtractedValue) public {
-    uint oldValue = _allowances[msg.sender][spender];
-    if (subtractedValue > oldValue) {
-      _allowances[msg.sender][spender] = 0;
-    } else {
-      _allowances[msg.sender][spender] = oldValue.sub(subtractedValue);
-    }
-    emit Approval(msg.sender, spender, _allowances[msg.sender][spender]);
-  }
-
-  function mint(address to, uint256 value) public onlyOwner {
-    _balances[to] = _balances[to].add(value);
-    _totalSupply = _totalSupply.add(value);
-    emit Transfer(0x0, to, value);
-  }
 }
--- contracts/test/Token_V1.sol
+++ contracts/test/Token_V1.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.21;
 
+import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
 import "./Token_V0.sol";
 
 
@@ -9,13 +10,4 @@ import "./Token_V0.sol";
  * The idea here is to extend a token behaviour providing burnable functionalities
  * in addition to what's provided in version 0
  */
-contract Token_V1 is Token_V0 {
-  event Burn(address indexed burner, uint256 value);
-
-  function burn(uint256 value) public {
-    require(balanceOf(msg.sender) >= value);
-    _balances[msg.sender] = _balances[msg.sender].sub(value);
-    _totalSupply = _totalSupply.sub(value);
-    emit Burn(msg.sender, value);
-  }
-}
+contract Token_V1 is Token_V0, BurnableToken {}
